264e8e9500f21a2d8412861a1d26a9eafd67c258
FatimaAryam <aryamfatima13@gmail.com>
2024-11-02 22:19:31 +0500

    updated
    updated

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def landing():
-    return render_template("landing.html")
+@app.route('/')
+def home():
+    return "Hello, Vercel!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
