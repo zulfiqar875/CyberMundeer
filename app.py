9ea0c3e248efbe7843e7f07f30e8edf782a3b7dd
FatimaAryam <aryamfatima13@gmail.com>
2024-11-02 22:21:29 +0500

    Fix
    Fix

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Vercel!"
+@app.route("/")
+def landing():
+    return render_template("landing.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
